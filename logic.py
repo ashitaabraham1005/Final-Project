4ac16ef0b41261803e6afcd3c5c599ca84d85087
ashitaabraham1005 <151096545+ashitaabraham1005@users.noreply.github.com>
2023-12-08 19:38:45 -0600

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,56 +1,67 @@
-import csv
+from PyQt6.QtWidgets import *
+from gui import *
 
 
-class VotingLogic:
+class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        '''
-        Initialize the VotingLogic object with candidates, votes, and load existing votes from a CSV file.
-        '''
-        self.candidates = ['Cameron', 'Allison', 'Diego']
-        self.votes = [0, 0, 0]
-        self.load_votes()
+        """
+        Initialize the Logic class.
 
-    def vote(self, candidate_index):
-        '''
-        Register a vote for the specific candidate.
+        This class inherits from QMainWindow and sets up the UI using the setupUi method.
 
-        :param candidate_index: Index of the candidate to vote for.
-        '''
-        self.votes[candidate_index] += 1
-        self.save_votes()
+        """
+        super().__init__()
+        self.setupUi(self)
+        self.__candidate_cbox = self.candidate_comboBox
+        self.summary_label.setWordWrap(True)
+        self.vote_radiobtn.clicked.connect(lambda: self.candidates_display())
+        self.vote_button.clicked.connect(lambda: self.vote(self.__candidate_cbox))
+        # self.exit_radiobtn.clicked.connect(lambda: self.vote(self.__candidate_cbox))
 
-    def get_results(self):
-        '''
-        Get the voting results as a list of strings.
+    def candidates_display(self):
+        """
+        Enable widgets for candidate selection.
 
-        :return:
-        -List[str]: A list of strings representing the results for each candidate.
-        '''
-        results = []
-        for candidate, vote_count in zip(self.candidates, self.votes):
-            results.append(f'{candidate}: {vote_count}')
-        return results
+        This method is connected to the vote_radiobtn and enables the candidate combo box, candidates label,
+        vote button, and summary label.
 
-    def load_votes(self):
-        '''
-        Load existing votes from a CSV file, if available.
-        '''
-        try:
-            with open('votes.csv', 'r') as file:
-                reader = csv.reader(file)
-                try:
-                    self.votes = [int(vote) for vote in next(reader)]
-                except StopIteration:
-                    # Handle the case where the file is empty
-                    pass
-        except FileNotFoundError:
-            # Handle the case where the file doesn't exist yet
-            pass
+        """
+        self.__candidate_cbox.setEnabled(True)
+        self.candidates_label.setEnabled(True)
+        self.vote_button.setEnabled(True)
+        self.summary_label.setEnabled(True)
 
-    def save_votes(self):
-        '''
-        Save the current votes to a CSV file.
-        '''
-        with open('votes.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(map(str, self.votes))
+    def vote(self, candidate):
+        """
+        Register a vote for the specific candidate and update the count on the GUI.
+
+        :param candidate: Name of the candidate to vote for.
+        """
+        self.title_label.setText('')
+        candidate_text = candidate.currentText()
+        if self.vote_button.clicked:  # Corrected the function call
+            candidates = {'Allison', 'Cameron', 'Sam', 'Diego'}
+            if candidate_text in candidates:
+                # Assuming vote_counts is an instance attribute
+                if not hasattr(self, 'vote_counts'):
+                    self.vote_counts = {'Allison': 0, 'Cameron': 0, 'Sam': 0, 'Diego': 0}
+
+                # Increment the vote for the chosen candidate
+                self.vote_counts[candidate_text] += 1
+
+                # Display the updated summary
+                summary_text = ""
+                for candidate_text, vote_count in self.vote_counts.items():
+                    summary_text += f"{candidate_text}: {vote_count}\n"
+
+                self.summary_label.setText(summary_text)
+
+                # Disable other widgets
+                self.__candidate_cbox.setEnabled(False)
+                self.candidates_label.setEnabled(False)
+                self.vote_button.setEnabled(False)
+                self.summary_label.setEnabled(True)
+
+        elif self.exit_radiobtn.clicked:
+            self.vote_counts = {'Allison': 0, 'Cameron': 0, 'Sam': 0, 'Diego': 0}
+            return self.title_label.pyqtConfigure(text='Summary')
